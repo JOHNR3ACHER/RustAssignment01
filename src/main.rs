b62344f056ed1170967aa2b695dc7134005c1903
JesusGracia <100613686+JOHNR3ACHER@users.noreply.github.com>
2024-02-07 05:41:45 +0000

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+
+mod ownerborrow;
+
+fn main(){
+
+    let name = "Jesus Gracia";
+    let course = "CSCI-3334-01-Spring2024";
+
+    println!("Assignment: Ownership and Borrowing in Rust");
+    println!("{}",name);
+    println!("{}",course);
+
+
+
+}
